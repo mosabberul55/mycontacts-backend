a07d58b12ffb932e059fb183578290e427098ed9
mosabberul55 <mosabberulislam55@gmail.com>
2024-08-22 14:49:35 +0600

    comment added

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,19 +5,27 @@
 const getContacts = (req, res) => {
     res.status(200).json({ message: "GET request to the homepage" });
 }
-
+//@desc Create a contact
+//@route POST /api/contacts
+//@access Public
 const createContact = (req, res) => {
     res.status(201).json({ message: "POST request to the homepage" });
 }
-
+//@desc Get a contact
+//@route GET /api/contacts/:id
+//@access Public
 const getContact = (req, res) => {
     res.status(200).json({ message: `GET request to the homepage with id ${req.params.id}` });
 }
-
+//@desc Update a contact
+//@route PUT /api/contacts/:id
+//@access Public
 const updateContact = (req, res) => {
-    res.status(200).json({ message: `PUT request to the homepage with id ${req.params.id}` });
+    res.status(201).json({ message: `PUT request to the homepage with id ${req.params.id}` });
 }
-
+//@desc Delete a contact
+//@route DELETE /api/contacts/:id
+//@access Public
 const deleteContact = (req, res) => {
     res.status(200).json({ message: `DELETE request to the homepage with id ${req.params.id}` });
 }
